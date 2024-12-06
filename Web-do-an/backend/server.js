44d90b77d7da9a5c4978745e54f149ca6ff9cb00
Hugn02 <29112002.hn@gmail.com>
2024-12-06 17:34:20 +0700

    update login/register

--- Web-do-an/backend/server.js
+++ Web-do-an/backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import productRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config'
 
 // App config
 const app = express();
@@ -16,6 +18,8 @@ connectDB();
 
 // API endpoint
 app.use("/api/product", productRouter);
+app.use("images",express.static('uploads'));
+app.use("/api/user",userRouter)
 
 // Default route
 app.get("/", (req, res) => {
