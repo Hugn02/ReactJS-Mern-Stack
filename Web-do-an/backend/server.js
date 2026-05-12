1a3e656bee169db187bc0ed5096d7da52a2b6ea4
Hugn02 <29112002.hn@gmail.com>
2026-05-12 10:28:03 +0700

    fix cors

--- Web-do-an/backend/server.js
+++ Web-do-an/backend/server.js
@@ -15,7 +15,15 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://web-react-js-front-end.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // DB connection
